3e4775a18dc95127d7ba718d961deb27b0785baf
Polina Nikulina <nikkulina.polly@gmail.com>
2026-03-30 10:23:22 +0300

    Create sample1.py

---
+++ 24/sample1.py
@@ -0,0 +1,12 @@
+f = open('input24.txt', 'r')
+data = f.read()
+data1 = data
+it1, it2 = 'PR', 'ST'
+s1 = data.replace(it1, it1[0] + ' ' + it1[1])
+s2 = data1.replace(it2, it2[0] + ' ' + it2[1])
+
+ans1 = max(s1.split(), key = len)
+ans2 = max(s2.split(), key = len)
+
+print(len(ans1), ans1)
+print(len(ans2), ans2)
