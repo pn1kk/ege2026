0c2bb623966f7b4a3aadcf1828f3e128e4b6636f
Polina Nikulina <nikkulina.polly@gmail.com>
2026-03-31 09:47:54 +0300

    Create sampledbscan.py

---
+++ 27/sampledbscan.py
@@ -0,0 +1,56 @@
+from math import dist
+
+def dbscan(dots):
+    eps = 0.5
+    all_clusters = []
+
+    while len(dots) > 0:
+        X = dots.pop()
+        new_cluster = [X]
+
+        for d in new_cluster:
+            neighbours = []
+
+            for d2 in new_cluster:
+                if dist(d, d2) <= eps:
+                    neighbours += [d2]
+        
+            new_cluster += neighbours
+
+            for d2 in neighbours:
+                dots.remove(d2)
+
+        all_clusters += [new_cluster]
+
+    return all_clusters
+
+def center(cluster):
+    m_sums = []
+    m_dots = []
+    for p in cluster:
+        s = 0
+        for p1 in cluster:
+            s += dist(p, p1)
+        m_sums.append(s)
+        m_dots.append(p)
+    return m_dots[m_sums.index(min(m_sums))]
+
+dots = []
+f = open('27_A.txt')
+
+for s in f:
+    x, y = [float(d) for d in s.replace(',', '.').split()]
+    dots.append([x, y])
+
+clusters = dbscan(dots)
+centers = [center(cl) for cl in clusters]
+
+sumx = 0
+sumy = 0
+
+for i in centers:
+    sumx += centers[0]
+    sumy += centers[1]
+
+print([len(i) for i in clusters])
+print(sumx/len(centers) * 1000, sumy/len(centers) * 1000)
